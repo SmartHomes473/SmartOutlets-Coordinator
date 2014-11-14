2c9c5936696f280be445409627682798990cbf1f
Nick Jurgens <njurgens@umich.edu>
2014-11-14 13:01:32 -0500

    Add RFM12B_tx() to header

--- drivers/rfm12b.h
+++ drivers/rfm12b.h
@@ -8,10 +8,15 @@
 #ifndef RFM12B_H_
 #define RFM12B_H_
 
+#include <stddef.h>
+#include <stdint.h>
+
 #define RFM12B_MAX_PAYLOAD		16
 #define RFM12B_PREAMBLE_LEN		2
 #define RFM12B_HEADER_LEN		8
 #define RFM12B_POSTAMBLE_LEN	2
 #define RFM12B_MAX_PACKET_LEN	28
+
+void RFM12B_tx ( const uint8_t *data, size_t len );
 
 #endif /* RFM12B_H_ */
