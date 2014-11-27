b426bb83be41fc827f719e84d23f136bf2031e69
Nick Jurgens <njurgens@umich.edu>
2014-11-26 20:46:48 -0500

    Add outlet webpage

--- www/outletctl.php
+++ www/outletctl.php
@@ -1,18 +1,25 @@
 <?php
-function write_database ( $ex_status )
+function write_database ( $ex_status, $state=NULL, $id=NULL )
 {
 	$len = strlen($ex_status);
 
 	$db = new mysqli('localhost', 'root', 'smarthouse', 'outlets');
 
 	if ($db->connect_errno > 0) {
-		die("Error connection to database: " . $db->connect_error);
+		die('Error connection to database: ' . $db->connect_error);
 	}
 
-	$query = $db->prepare("UPDATE `Communication` SET `Status` = 1, `ExStatusLength` = ?, `ExtendedStatus` = ?");
-
-	$query->bind_param("is", $len, $ex_status);
+	// update status
+	$query = $db->prepare('UPDATE `Communication` SET `Status` = 1, `ExStatusLength` = ?, `ExtendedStatus` = ?');
+	$query->bind_param('is', $len, $ex_status);
 	$query->execute();
+
+	// optionally update the state in the status table
+	if (!is_null($state) && is_numeric($id)) {
+		$query = $db->prepare('UPDATE `Outlets` SET `state` = ? WHERE `id` = ?');
+		$query = $db->bind_param('si', $state, $id);
+		$query->execute();
+	}
 }
 
 function exit_with_400 ( )
@@ -33,13 +40,13 @@ function get_or_die ( $key, $array )
 function outlet_on ( $outlet_id )
 {
 	$ex_status = chr($outlet_id) + chr(0x11);
-	write_database($ex_status);
+	write_database($ex_status, 'on', $outlet_id);
 }
 
 function outlet_off ( $outlet_id )
 {
 	$ex_status = chr($outlet_id) + chr(0x22);
-	write_database($ex_status);
+	write_database($ex_status, 'off', $outlet_id);
 }
 
 function outlet_get_power ( $outlet_id )
